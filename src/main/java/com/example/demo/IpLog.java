5a2463655e529329f8bd91ae274b4eecf44c8f56
jaykarnik7 <jaykarnik@gmail.com>
2025-08-03 19:14:13 +0530

    Rehaul Log file

--- src/main/java/com/example/demo/IpLog.java
+++ src/main/java/com/example/demo/IpLog.java
@@ -5,36 +5,46 @@ import java.time.LocalDateTime;
 
 @Entity
 public class IpLog {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String ip;
     private LocalDateTime timestamp;
-    // getters/setters omitted for brevity
 
+    // Getters and Setters
 
-public String getName() {
-    return name;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
 }
-
-public void setName(String name) {
-    this.name = name;
-}
-
-public String getIp() {
-    return ip;
-}
-
-public void setIp(String ip) {
-    this.ip = ip;
-}
-
-public LocalDateTime getTimestamp() {
-    return timestamp;
-}
-
-public void setTimestamp(LocalDateTime timestamp) {
-    this.timestamp = timestamp;
-}
-)
