3c2a1c02df598c9befc3c89cdca20f22274d5adf
jaykarnik7 <47596680+jaykarnik7@users.noreply.github.com>
2025-08-03 19:00:24 +0530

    Log Changes

--- src/main/java/com/example/demo/IpLog.java
+++ src/main/java/com/example/demo/IpLog.java
@@ -13,3 +13,27 @@ public class IpLog {
     private LocalDateTime timestamp;
     // getters/setters omitted for brevity
 }
+
+public String getName() {
+    return name;
+}
+
+public void setName(String name) {
+    this.name = name;
+}
+
+public String getIp() {
+    return ip;
+}
+
+public void setIp(String ip) {
+    this.ip = ip;
+}
+
+public LocalDateTime getTimestamp() {
+    return timestamp;
+}
+
+public void setTimestamp(LocalDateTime timestamp) {
+    this.timestamp = timestamp;
+}
